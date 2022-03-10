903306ea4b72669db7c6e3cdfeb84ec30152e7ac
EC2 Default User <ec2-user@ip-172-31-14-73.ec2.internal>
2022-03-10 19:49:25 +0000

    transformed sentences to embeddings

--- s_transformer.py
+++ s_transformer.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from sentence_transformers import SentenceTransformer
 
-df = '/home/ec2-user/environment/ACA_Public/joe_dutch_clean.xlsx'
+path = '/home/ec2-user/environment/ACA_Public/joe_dutch_clean.xlsx'
+
+df = pd.read_excel(path)
 
 sentences = df['text'].tolist()
 
@@ -11,4 +13,6 @@ model = SentenceTransformer('all-mpnet-base-v2')
 
 embeddings = model.encode(sentences)
 
-print(type(embeddings))
+embeddings_df = pd.DataFrame(data=embeddings)
+
+pd.DataFrame.to_excel(embeddings_df, '/home/ec2-user/environment/ACA_Public/embeddings_all-mpnet-base-v2.xlsx')
