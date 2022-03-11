3258cfb3fc80f89463b90a7c51c3b3fde648a3be
Endomorphin <74832024+Endomorphin-lab@users.noreply.github.com>
2022-03-11 12:37:51 +0100

    Multilingual transformer - retest2

--- s_transformer_multilingual.py
+++ s_transformer_multilingual.py
@@ -1,26 +1,18 @@
 import pandas as pd
-from transformers import AutoTokenizer, AutoModel
+from sentence_transformers import SentenceTransformer
 
-path = '/Users/nizarmichaud/PycharmProjects/ACA_Public/joe_dutch_clean.xlsx'
+path = '/home/ec2-user/environment/ACA_Public/joe_dutch_clean.xlsx'
 
 df = pd.read_excel(path)
 
 sentences = df['text'].tolist()
 
-# model
+#model
 
-tokenizer = AutoTokenizer.from_pretrained("DeepPavlov/bert-base-multilingual-cased-sentence")
+model = SentenceTransformer('distiluse-base-multilingual-cased')
 
-model = AutoModel.from_pretrained("DeepPavlov/bert-base-multilingual-cased-sentence")
-
-data_tokenized = tokenizer.tokenize(sentences)
-
-encoded_input = tokenizer(sentences, return_tensors='pt', padding=True)
-
-data_input = tokenizer.tokenize(sentences)
-
-embeddings = model(**encoded_input).last_hidden_state.detach().numpy()[0]
+embeddings = model.encode(sentences)
 
 embeddings_df = pd.DataFrame(data=embeddings)
 
-pd.DataFrame.to_excel(embeddings_df, '/home/ec2-user/environment/ACA_Public/BERT-multilingual.xlsx')
+pd.DataFrame.to_excel(embeddings_df, '/home/ec2-user/environment/ACA_Public/multilingual-embeddings.xlsx')
